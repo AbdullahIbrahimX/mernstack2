6f16b60a23dba3597456c657e61e0609b08fa660
SafLabs <alsafwan.abdullah@gmail.com>
2021-04-11 01:08:34 +0300

    11/4/2021

--- config/socket.js
+++ config/socket.js
@@ -3,12 +3,19 @@ module.exports = (io)=>{
         console.log(socket.id);
 
         socket.on("messageSent", socket =>{
-           console.log(socket)
-            io.emit('newMessage')
+           console.log('Socket log')
+           console.log( socket );
+            io.emit('newMessage');
+        });
+
+        socket.on('test',()=>{
+            console.log('test Googd');
+            io.emit('test1');
+        })
+        socket.on('disconnect', socket=>{
+            console.log(socket.id + "  Disconnected");
         });
 
     });
-    io.on('disconnection', socket=>{
-        console.log(socket.id + "  Disconnected");
-    });
+
 };
