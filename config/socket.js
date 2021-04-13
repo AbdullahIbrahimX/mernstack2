3b2537378da963f463d6c63c44d0967f4f135d4a
SafLabs <alsafwan.abdullah@gmail.com>
2021-04-13 13:22:35 +0300

    13/4/2021

--- config/socket.js
+++ config/socket.js
@@ -1,22 +1,39 @@
 module.exports = (io)=>{
-    io.on('connection', socket=>{
-        console.log(socket.id);
-
-        socket.on("messageSent", socket =>{
-           console.log('Socket log')
-           console.log( socket );
-            io.emit('newMessage');
+    io.of('/test').on('connection',test =>{
+        console.log('test is good');
+        io.of('/test').emit('test1');
+        test.on('disconnect',()=>{
+            console.log('Test disconnected')
         });
+        test.on('test',()=>{
+            console.log('test Googd');
+            io.emit('test1');
+        });
+    });
+    io.on('connection', socket=>{
+    console.log(socket.id);
 
+    socket.on("messageSent", socket =>{
+       console.log('Socket log')
+       console.log( socket );
+        io.emit('newMessage');
+    });
+    ///////////////////////////
+    //TODO for testing only >>> to be deleted
         socket.on('test',()=>{
             console.log('test Googd');
             io.emit('test1');
         });
 
-        socket.on('disconnect', socket=>{
-            console.log(socket.id + "  Disconnected");
-        });
+    //////////////////////////
 
+
+
+
+    socket.on('disconnect', socket=>{
+        console.log(socket.id + "  Disconnected");
     });
 
+});
 };
+
