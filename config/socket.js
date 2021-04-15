83a12b676a60318a637c8d644957986c898f81af
SafLabs <alsafwan.abdullah@gmail.com>
2021-04-15 12:03:02 +0300

    15/4/2021

--- config/socket.js
+++ config/socket.js
@@ -1,39 +1,71 @@
+const logger = require('morgan');
+const passport = require('passport');
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+
+
 module.exports = (io)=>{
-    io.of('/test').on('connection',test =>{
-        console.log('test is good');
-        io.of('/test').emit('test1');
-        test.on('disconnect',()=>{
-            console.log('Test disconnected')
-        });
-        test.on('test',()=>{
-            console.log('test Googd');
-            io.emit('test1');
-        });
-    });
-    io.on('connection', socket=>{
-    console.log(socket.id);
+    //----middleware----//
+    io.use(wrap(logger('dev')));
+    io.use(wrap(passport.initialize()));
+    io.use(wrap(passport.session()));
+    require('../config/passport')(passport);
 
-    socket.on("messageSent", socket =>{
-       console.log('Socket log')
-       console.log( socket );
-       io.emit('newMessage',socket.data);
+
+    //----NSPs----//
+
+
+    io.of('/').use((socket,next)=>{
+        console.log(socket.id);
+        socket.on('disconnect',()=>{
+           console.log('User Disconnected');
+           next();
+        });
+        next();
     });
-    ///////////////////////////
-    //TODO for testing only >>> to be deleted
+
+    io.of('/test').use((socket,next)=>{
         socket.on('test',()=>{
-            console.log('test Good');
-            io.emit('test1');
+            console.log('test is good');
         });
-
-    //////////////////////////
-
-
-
-
-    socket.on('disconnect', socket=>{
-        console.log(socket.id + "  Disconnected");
-    });
-
-});
+        socket.emit('test1');
+        socket.on('disconnect',()=>{
+            console.log('Test Disconnected');
+        });
+        next();
+    })
 };
 
+//io.on('connection', socket=>{
+//         console.log(socket.id);
+//
+//         socket.on("messageSent", socket =>{
+//           console.log('Socket log')
+//           console.log( socket );
+//           io.emit('newMessage');
+//     });
+//
+//
+//     ///////////////////////////
+//     //TODO for testing only >>> to be deleted
+//     socket.on('test',()=>{
+//             console.log('test Googd');
+//             io.emit('test1');
+//         });
+//     io.of('/test').on('connection',test =>{
+//         console.log('test is good');
+//         io.of('/test').emit('test1');
+//         test.on('disconnect',()=>{
+//             console.log('Test disconnected')
+//         });
+//         test.on('test',()=>{
+//             console.log('test Googd');
+//             io.emit('test1');
+//         });
+//     });
+//     //////////////////////////
+//
+//     socket.on('disconnect', socket=>{
+//         console.log(socket.id + "  Disconnected");
+//     });
+//
+// });
